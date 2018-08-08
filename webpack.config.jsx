416804086a30ef033f9801c7cc6c8391370e5d1d
Amanda <a@zalee.me>
2018-08-07 22:24:23 -0700

    Complete entry file, webpack.config.js, and static pages setup

---
+++ webpack.config.jsx
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/leeway.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
