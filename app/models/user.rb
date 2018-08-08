14f30d051e7532d7fb90d6939fcf0c7050c873ad
Amanda <a@zalee.me>
2018-08-07 22:46:29 -0700

    Add users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
