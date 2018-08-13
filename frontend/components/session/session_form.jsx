44d0b93d047a0e0dd19beccb926a32580a318602
Amanda <a@zalee.me>
2018-08-13 13:53:39 -0700

    Add Guest Login button directly on splash page when logged out

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -121,19 +121,4 @@ class SessionForm extends React.Component {
   }
 }
 
-import { connect } from 'react-redux';
-import { login, receiveErrors } from '../../actions/session_actions';
-import SessionForm from './session_form';
-
-const mapStateToProps = ({ errors }) => ({
-  errors: errors.session,
-  formHeader: 'Log in to Leeway',
-  formType: 'Log In'
-});
-
-const mapDispatchToProps = dispatch => ({
-  processForm: (user) => dispatch(login(user)),
-  receiveErrors: (errors) => dispatch(receiveErrors(errors))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
+export default SessionForm;
--- frontend/components/splash/splash_content.jsx
+++ frontend/components/splash/splash_content.jsx
@@ -2,43 +2,68 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HeaderContainer from './header_container';
 
-const SplashContent = () => {
-  return(
-    <div className="splash">
+class SplashContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.demo = this.demo.bind(this);
+  }
 
-      <HeaderContainer />
+  demo() {
+    return this.props.guestLogin().then( () => {
+      this.props.history.push('/workspace');
+    });
+  }
 
-      <div className="splash-content">
-        <div className ="splash-picture-col">
-          <img id="splash-illustration" src={window.images.splashpic}/>
+  render() {
+    const { currentUser } = this.props;
+    return(
+      <div className="splash">
+
+        <HeaderContainer />
+
+        <div className="splash-content">
+          <div className ="splash-picture-col">
+            <img id="splash-illustration" src={window.images.splashpic}/>
+          </div>
+
+          <div className="text-col">
+            <h1>Where Work Happens</h1>
+
+            <p className="blurb">
+              When your team needs to kick off a project, hire a new
+              employee, deploy some code, review a sales contract, finalize
+              next year's budget, measure an A/B test, plan your next office
+              opening, and more, Leeway has you covered.
+            </p>
+
+            <Link className="get-started-button" to="signup">Get Started</Link>
+              {
+                currentUser ? (
+                  <div></div>
+                ) : (
+                  <button className="guest-login-button" onClick={this.demo}>Guest Login</button>
+                )
+              }
+
+            <p> Already using Leeway? <Link className="signin-link" to="/signin">Sign in</Link>.</p>
+          </div>
         </div>
 
-        <div className="text-col">
-          <h1>Where Work Happens</h1>
-          <p className="blurb">
-            When your team needs to kick off a project, hire a new
-            employee, deploy some code, review a sales contract, finalize
-            next year's budget, measure an A/B test, plan your next office
-            opening, and more, Leeway has you covered.
-          </p>
-          <Link className="get-started-button" to="signup">Get Started</Link>
-          <p> Already using Leeway? <Link className="signin-link" to="/signin">Sign in</Link>.</p>
-        </div>
       </div>
-
-    </div>
-  );
-};
+    );
+  }
+}
 
 
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
 
 const mapStateToProps = (state) => ({
+  currentUser: state.entities.users[state.session.id]
 });
 
 const mapDispatchToProps = dispatch => ({
-  guestLogin: (user) => dispatch(login(user))
+  guestLogin: () => dispatch( login({username: "harry_potter", password: "123456"}) )
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SplashContent);
