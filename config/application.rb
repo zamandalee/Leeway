a90cc62acd1b2a901f2ca6a058872690a918211c
Amanda <a@zalee.me>
2018-08-13 18:20:48 -0700

    Remove both favicon and custom font to debug Heroku

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Leeway
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
   end
 end
