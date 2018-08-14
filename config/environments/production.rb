3fb34b7c4e0ee9c8a8cfa55a36701a9cb52ec944
Amanda <a@zalee.me>
2018-08-13 19:12:26 -0700

    Add Action Cable config line to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.action_cable.allowed_request_origins = ["https://leewayapp.herokuapp.com"]
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
